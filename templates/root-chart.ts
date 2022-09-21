e21373b5c1052cb0e053c67b237ab86387a4b6fc
Matt Johnston <m@ttjohnston.com>
2022-09-21 20:26:40 -0300

    delete extra templates folder

--- templates/root-chart.ts
+++
@@ -1,7 +0,0 @@
-const ChartYaml = `apiVersion: v2
-name: root-application
-description: top most application used to connect all the other applications
-type: application
-version: 1.0.0
-appVersion: "1.0.0"`
-export default ChartYaml
