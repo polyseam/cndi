add7ec5fe57cd57dacb2997d91dc7fa03feeab3d
Matt Johnston <m@ttjohnston.com>
2022-09-28 15:45:07 -0300

    new packer build

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -54,7 +54,7 @@ import getRepoConfigManifest from "../templates/repo-config.ts";
 const DEFAULT_AWS_EC2_API_VERSION = "2016-11-15";
 const DEFAULT_AWS_REGION = "us-east-1";
 const DEFAULT_AWS_INSTANCE_TYPE = "t2.micro";
-const DEFAULT_AWS_IMAGE_ID = "ami-0180ef4f54678af5f";
+const DEFAULT_AWS_IMAGE_ID = "ami-06e67c2c137e90884";
 
 const DEFAULT_BOOT_DISK_VOLUME_GIB = 80;
 
