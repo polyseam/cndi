bc7c1122bec6b7d9795cb04bbf302e428e5f8cf6
Matt Johnston <m@ttjohnston.com>
2022-10-12 19:01:24 -0300

    self-destruct if accept-invite is not replaced

--- src/bootstrap/worker/accept-invite.sh
+++ src/bootstrap/worker/accept-invite.sh
@@ -2,7 +2,7 @@
 
 # This file will be copied to CNDI_HOME/src/bootstrap/worker/accept-invite.sh
 # It will never be executed or templated, only replaced.
-
-exit 1
+echo "accept-invite called untemplated"
+exit 191
 echo "accepting node invite with token _TOKEN_"
 microk8s join _IP_ADDRESS_:25000/_TOKEN_ --worker
