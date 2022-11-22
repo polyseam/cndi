d3ec067eccc4e30bbd407496b25c53c0c173628f
Matt Johnston <m@ttjohnston.com>
2022-11-22 18:56:12 -0400

    fmt

--- src/setTF_VARs.ts
+++ src/setTF_VARs.ts
@@ -7,25 +7,28 @@ export default function setTF_VARs() {
   const git_username = Deno.env.get("GIT_USERNAME");
 
   if (!git_username) {
-    console.log(setTF_VARsLabel, brightRed('GIT_USERNAME env var is not set'))
+    console.log(setTF_VARsLabel, brightRed("GIT_USERNAME env var is not set"));
     Deno.exit(1);
   }
 
   const git_password = Deno.env.get("GIT_PASSWORD");
   if (!git_password) {
-    console.log(setTF_VARsLabel, brightRed('GIT_PASSWORD env var is not set'))
+    console.log(setTF_VARsLabel, brightRed("GIT_PASSWORD env var is not set"));
     Deno.exit(1);
   }
 
   const git_repo = Deno.env.get("GIT_REPO");
   if (!git_repo) {
-    console.log(setTF_VARsLabel, brightRed('GIT_REPO env var is not set'))
+    console.log(setTF_VARsLabel, brightRed("GIT_REPO env var is not set"));
     Deno.exit(1);
   }
 
   const argo_ui_readonly_password = Deno.env.get("ARGO_UI_READONLY_PASSWORD");
   if (!argo_ui_readonly_password) {
-    console.log(setTF_VARsLabel, brightRed('ARGO_UI_READONLY_PASSWORD env var is not set'))
+    console.log(
+      setTF_VARsLabel,
+      brightRed("ARGO_UI_READONLY_PASSWORD env var is not set"),
+    );
     Deno.exit(1);
   }
 
@@ -35,7 +38,10 @@ export default function setTF_VARs() {
     .replaceAll("_", "\n");
 
   if (!sealed_secrets_private_key_material) {
-    console.log(setTF_VARsLabel, brightRed('SEALED_SECRETS_PRIVATE_KEY_MATERIAL env var is not set'))
+    console.log(
+      setTF_VARsLabel,
+      brightRed("SEALED_SECRETS_PRIVATE_KEY_MATERIAL env var is not set"),
+    );
     Deno.exit(1);
   }
 
@@ -43,14 +49,19 @@ export default function setTF_VARs() {
     .get("SEALED_SECRETS_PUBLIC_KEY_MATERIAL")
     ?.trim()
     .replaceAll("_", "\n");
+
   if (!sealed_secrets_public_key_material) {
-    console.log(setTF_VARsLabel, brightRed('SEALED_SECRETS_PUBLIC_KEY_MATERIAL env var is not set'))
+    console.log(
+      setTF_VARsLabel,
+      brightRed("SEALED_SECRETS_PUBLIC_KEY_MATERIAL env var is not set"),
+    );
     Deno.exit(1);
   }
 
   const sealed_secrets_private_key = padPrivatePem(
     sealed_secrets_private_key_material,
   );
+
   const sealed_secrets_public_key = padPublicPem(
     sealed_secrets_public_key_material,
   );
