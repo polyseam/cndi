67858542a9ed3c61d7221dfb6609137992dfe40c
Matt Johnston <m@ttjohnston.com>
2023-03-01 12:28:56 -0400

    add basic mock cndi-config

---
+++ src/tests/mocks/cndiConfigs.ts
@@ -0,0 +1,21 @@
+const basicCndiConfig = {
+  cndi_version: "v1",
+  project_name: "my-cndi-project",
+  infrastructure: {
+    cndi: {
+      nodes: [
+        {
+          name: "x-node",
+          kind: "aws",
+          role: "leader",
+          instance_type: "m5a.2xlarge",
+          volume_size: 128,
+        },
+      ],
+    },
+  },
+  cluster_manifests: {},
+  applications: {},
+};
+
+export { basicCndiConfig };
