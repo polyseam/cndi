32bb649cd821fa4ed94b695635410bc88a1789e6
IamTamika <tamika.taylor@untribe.com>
2023-03-08 21:32:50 -0700

    fix target group attach

--- src/outputs/terraform/aws/cndi_aws_lb_target_group_attachment.tf.json.ts
+++ src/outputs/terraform/aws/cndi_aws_lb_target_group_attachment.tf.json.ts
@@ -1,6 +1,6 @@
 import {
   getPrettyJSONString,
-  getTFResource,
+
 } from "src/utils.ts";
 import {
   AWSNodeItemSpec
@@ -11,22 +11,22 @@ export default function getAWSComputeInstanceTFJSON(
 ): string {
   const { name } = node;
   const target_id = `\${aws_instance.cndi_aws_instance_${name}.id}`;
-  const resource = getTFResource(
-    "aws_lb_target_group_attachment", {
-    [`cndi_aws_lb_target_group_attachment_https_${name}`]: [
-      {
-        target_group_arn: "${aws_lb_target_group.cndi_aws_lb_target_group_https.arn}",
-        target_id,
-      },
-    ],
-    [`cndi_aws_lb_target_group_attachment_http_${name}`]: [
-      {
-        target_group_arn: "${aws_lb_target_group.cndi_aws_lb_target_group_http.arn}",
-        target_id,
-      },
-    ],
-  }).resource;
-
+  const resource = {
+    [`aws_lb_target_group_attachment_${name}`]: {
+      [`cndi_aws_lb_target_group_attachment_https_${name}`]: [
+        {
+          target_group_arn: "${aws_lb_target_group.cndi_aws_lb_target_group_https.arn}",
+          target_id,
+        },
+      ],
+      [`cndi_aws_lb_target_group_attachment_http_${name}`]: [
+        {
+          target_group_arn: "${aws_lb_target_group.cndi_aws_lb_target_group_http.arn}",
+          target_id,
+        },
+      ],
+    }
+  }
   return getPrettyJSONString({
     resource
   });
