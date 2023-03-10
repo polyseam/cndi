9f9a327d64ad3792325cc9050dd7668cb1a20119
IamTamika <tamika.taylor@untribe.com>
2023-03-09 22:08:59 -0700

    ch res name to be the file name for the instances

--- src/outputs/terraform/gcp/stageAll.ts
+++ src/outputs/terraform/gcp/stageAll.ts
@@ -81,7 +81,7 @@ export default async function stageTerraformResourcesForGCP(
       path.join(
         "cndi",
         "terraform",
-        `${node.name}.cndi_google_compute_instance.tf.json`,
+        `cndi_google_compute_instance_${node.name}.tf.json`,
       ),
       cndi_google_compute_instance(node, config),
     )
@@ -92,7 +92,7 @@ export default async function stageTerraformResourcesForGCP(
       path.join(
         "cndi",
         "terraform",
-        `${node.name}.cndi_google_compute_disk.tf.json`,
+        `cndi_google_compute_disk_${node.name}.tf.json`,
       ),
       cndi_google_compute_disk(node),
     )
