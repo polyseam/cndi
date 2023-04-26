9e4953448e2c4c80258fa83c5125be32322b1777
IamTamika <tamika.taylor@untribe.com>
2023-04-25 22:54:38 -0600

    make cert man depend on nginx controller

--- src/outputs/terraform/aws-eks/cndi_cert_manager_helm_chart.tf.json.ts
+++ src/outputs/terraform/aws-eks/cndi_cert_manager_helm_chart.tf.json.ts
@@ -4,14 +4,14 @@ export default function getCertManagerTFJSON(): string {
   const resource = getTFResource("helm_release", {
     chart: "cert-manager",
     create_namespace: true,
-    depends_on: ["aws_eks_node_group.cndi_aws_eks_node_group"],
+    depends_on: ["aws_eks_node_group.cndi_aws_eks_node_group","helm_release.cndi_nginx_controller_helm_chart"],
     name: "cert-manager",
     namespace: "cert-manager",
     repository: "https://charts.jetstack.io",
     set: [{ name: "installCRDs", value: "true" }],
     version: "1.11.1",
-    timeout    : "600",
-    atomic     :true
+    timeout: "600",
+    atomic: true,
   }, "cndi_cert_manager_helm_chart");
   return getPrettyJSONString(resource);
 }
