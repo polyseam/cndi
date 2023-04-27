86902c437483e976596d064ac4f85459f222b1f3
IamTamika <tamika.taylor@untribe.com>
2023-04-26 21:51:36 -0600

    change to root

--- src/outputs/terraform/aws-eks/cndi_local_file_kubeconfig.tf.json.ts
+++ src/outputs/terraform/aws-eks/cndi_local_file_kubeconfig.tf.json.ts
@@ -2,7 +2,7 @@ import { getPrettyJSONString, getTFResource } from "src/utils.ts";
 export default function getLocalFileTFJSON(): string {
   const resource = getTFResource("local_file", {
     content: "${data.template_file.kubeconfig.rendered}",
-    filename: "${path.module}/.kube/config",
+    filename: "~/.kube/config",
   }, "cndi_local_file_kubeconfig");
   return getPrettyJSONString(resource);
 }
