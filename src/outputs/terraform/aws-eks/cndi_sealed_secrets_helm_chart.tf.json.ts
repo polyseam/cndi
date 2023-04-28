bd52a81650265b44bf696de7adccaa10aee070e1
IamTamika <tamika.taylor@untribe.com>
2023-04-28 01:16:44 -0600

    ch deps so that the secret key is default

--- src/outputs/terraform/aws-eks/cndi_sealed_secrets_helm_chart.tf.json.ts
+++ src/outputs/terraform/aws-eks/cndi_sealed_secrets_helm_chart.tf.json.ts
@@ -3,7 +3,7 @@ import { getPrettyJSONString, getTFResource } from "src/utils.ts";
 export default function getSealedSecretsTFJSON(): string {
   const resource = getTFResource("helm_release", {
     chart: "sealed-secrets",
-    depends_on: ["aws_eks_node_group.cndi_aws_eks_node_group"],
+    depends_on: ["aws_eks_node_group.cndi_aws_eks_node_group", "kubectl_manifest.cndi_sealed_secrets_secret_manifest"],
     name: "sealed-secrets",
     namespace: "kube-system",
     repository: "https://bitnami-labs.github.io/sealed-secrets",
--- src/outputs/terraform/aws-eks/cndi_sealed_secrets_secret_manifest.tf.json.ts
+++ src/outputs/terraform/aws-eks/cndi_sealed_secrets_secret_manifest.tf.json.ts
@@ -2,7 +2,7 @@ import { getPrettyJSONString, getTFResource } from "src/utils.ts";
 
 export default function getSecretSealedSecretsTFJSON(): string {
   const resource = getTFResource("kubectl_manifest", {
-    depends_on: ["helm_release.cndi_sealed_secrets_helm_chart"],
+    depends_on: ["aws_eks_node_group.cndi_aws_eks_node_group"],
     yaml_body: "${data.template_file.sealed_secrets_secret_manifest.rendered}",
   }, "cndi_sealed_secrets_secret_manifest");
   return getPrettyJSONString(resource);
