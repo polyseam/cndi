ef23ff8e159c46e301e1ef4752564b7f3ddf3ab5
Matt Johnston <m@ttjohnston.com>
2023-04-28 17:19:22 -0300

    deno task build

--- src/outputs/terraform/aws-eks/cndi_argocd_admin_password_secret_manifest.tf.json.ts
+++ src/outputs/terraform/aws-eks/cndi_argocd_admin_password_secret_manifest.tf.json.ts
@@ -3,7 +3,8 @@ import { getPrettyJSONString, getTFResource } from "src/utils.ts";
 export default function getSecretArgocdAdminPasswordManifestTFJSON(): string {
   const resource = getTFResource("kubectl_manifest", {
     depends_on: ["helm_release.cndi_argocd_helm_chart"],
-    yaml_body: "${data.template_file.argocd_admin_password_secret_manifest.rendered}",
+    yaml_body:
+      "${data.template_file.argocd_admin_password_secret_manifest.rendered}",
   }, "cndi_argocd_admin_password_secret_manifest");
   return getPrettyJSONString(resource);
 }
