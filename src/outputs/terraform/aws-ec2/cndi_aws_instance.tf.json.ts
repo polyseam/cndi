607fee862c4ca636f730772f2cf219b3756d62a1
IamTamika <tamika.taylor@untribe.com>
2023-05-19 17:41:24 -0600

    ch so vol is destroyed on instance termination

--- src/outputs/terraform/aws-ec2/cndi_aws_instance.tf.json.ts
+++ src/outputs/terraform/aws-ec2/cndi_aws_instance.tf.json.ts
@@ -10,7 +10,7 @@ export default function getAWSComputeInstanceTFJSON(
   const DEFAULT_EC2_AMI = "ami-0c1704bac156af62c";
   const ami = node?.ami || DEFAULT_EC2_AMI;
   const instance_type = node?.instance_type || DEFAULT_INSTANCE_TYPES.aws;
-  const delete_on_termination = false;
+  const delete_on_termination = true;
   const volume_size = node?.volume_size || node?.disk_size || node?.size ||
     node?.disk_size_gb || DEFAULT_NODE_DISK_SIZE; //GiB
   const volume_type = "gp3"; // general purpose SSD
