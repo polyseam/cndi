13fd3285e964517b9f8afec7dc6896ffd8e435e7
Matt Johnston <m@ttjohnston.com>
2023-06-05 22:28:01 -0300

    rg AllSupported

--- src/outputs/terraform/aws-eks/cndi_aws_resourcegroups_group.tf.json.ts
+++ src/outputs/terraform/aws-eks/cndi_aws_resourcegroups_group.tf.json.ts
@@ -4,6 +4,9 @@ export default function getAWSResourceGroupTFJSON(
   cndi_project_name?: string,
 ): string {
   const query = JSON.stringify({
+    ResourceTypeFilters: [
+      "AWS::AllSupported",
+    ],
     TagFilters: [
       {
         "Key": "CNDIProject",
