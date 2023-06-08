cff96c6cfd73def002faf6c90349a5bb06ff8833
Matt Johnston <m@ttjohnston.com>
2023-06-08 17:26:54 -0300

    fix outputs syntax

--- src/outputs/terraform/aws-ec2/cndi_outputs.tf.json.ts
+++ src/outputs/terraform/aws-ec2/cndi_outputs.tf.json.ts
@@ -1,15 +1,15 @@
 import { getPrettyJSONString } from "src/utils.ts";
 
 export default function getOutputTFJSON(): string {
-  const value = {
-    public_host: "${aws_lb.cndi_aws_lb.dns_name}",
-    resource_group:
-      "https://${local.aws_region}.console.aws.amazon.com/resource-groups/group/CNDIResourceGroup_${local.cndi_project_name}?region=${local.aws_region}",
-  };
-
   return getPrettyJSONString({
     output: {
-      value,
+      public_host: {
+        value: "${aws_lb.cndi_aws_lb.dns_name}",
+      },
+      resource_group: {
+        value:
+          "https://${local.aws_region}.console.aws.amazon.com/resource-groups/group/CNDIResourceGroup_${local.cndi_project_name}?region=${local.aws_region}",
+      },
     },
   });
 }
--- src/outputs/terraform/gcp/cndi_outputs.tf.json.ts
+++ src/outputs/terraform/gcp/cndi_outputs.tf.json.ts
@@ -1,16 +1,16 @@
 import { getPrettyJSONString } from "src/utils.ts";
 
 export default function getOutputTFJSON(): string {
-  const value = {
-    public_host:
-      "${google_compute_forwarding_rule.cndi_google_compute_forwarding_rule.ip_address}",
-    resource_group:
-      "https://console.cloud.google.com/welcome?project=${local.project_id}",
-  };
-
   return getPrettyJSONString({
     output: {
-      value,
+      public_host: {
+        value:
+          "${google_compute_forwarding_rule.cndi_google_compute_forwarding_rule.ip_address}",
+      },
+      resource_group: {
+        value:
+          "https://console.cloud.google.com/welcome?project=${local.project_id}",
+      },
     },
   });
 }
