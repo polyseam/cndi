714ccea8d8b9fdc8ebd1ce2d28b0a9edd7b49224
Matt Johnston <m@ttjohnston.com>
2023-06-08 17:19:15 -0300

    don't stringify values

--- src/outputs/terraform/aws-ec2/cndi_outputs.tf.json.ts
+++ src/outputs/terraform/aws-ec2/cndi_outputs.tf.json.ts
@@ -1,11 +1,11 @@
 import { getPrettyJSONString } from "src/utils.ts";
 
 export default function getOutputTFJSON(): string {
-  const value = JSON.stringify({
+  const value = {
     public_host: "${aws_lb.cndi_aws_lb.dns_name}",
     resource_group:
       "https://${local.aws_region}.console.aws.amazon.com/resource-groups/group/CNDIResourceGroup_${local.cndi_project_name}?region=${local.aws_region}",
-  });
+  };
 
   return getPrettyJSONString({
     output: {
--- src/outputs/terraform/azure/cndi_outputs.tf.json.ts
+++ src/outputs/terraform/azure/cndi_outputs.tf.json.ts
@@ -1,11 +1,11 @@
 import { getPrettyJSONString } from "src/utils.ts";
 
 export default function getOutputTFJSON(): string {
-  const value = JSON.stringify({
+  const value = {
     public_host: "${azurerm_public_ip_lb.cndi_azurerm_public_ip_lb.ip_address}", // TODO: Add public host
     resource_group:
       "https://portal.azure.com/#view/HubsExtension/BrowseResourcesWithTag/tagName/CNDIProject/tagValue/#${local.cndi_project_name}",
-  });
+  };
 
   return getPrettyJSONString({
     output: {
