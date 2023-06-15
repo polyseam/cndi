65b76950168d0646b04c79205389ac4efca8d40a
Matt Johnston <m@ttjohnston.com>
2023-06-15 17:18:40 -0300

    fix template urls

--- src/templates/knownTemplates.ts
+++ src/templates/knownTemplates.ts
@@ -6,66 +6,66 @@ export default function getKnownTemplates() {
     // ec2
     {
       name: "ec2/basic",
-      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/ec2/basic.json`,
+      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/ec2/basic.jsonc`,
     },
     {
       name: "ec2/airflow-cnpg",
-      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/ec2/airflow-cnpg.json`,
+      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/ec2/airflow-cnpg.jsonc`,
     },
     {
       name: "ec2/hop",
-      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/ec2/hop.json`,
+      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/ec2/hop.jsonc`,
     },
     {
       name: "ec2/cnpg",
-      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/ec2/cnpg.json`,
+      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/ec2/cnpg.jsonc`,
     },
     // eks
     {
       name: "eks/basic",
-      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/eks/basic.json`,
+      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/eks/basic.jsonc`,
     },
     {
       name: "eks/airflow-cnpg",
-      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/eks/airflow-cnpg.json`,
+      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/eks/airflow-cnpg.jsonc`,
     },
     {
       name: "eks/hop",
-      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/eks/hop.json`,
+      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/eks/hop.jsonc`,
     },
     // gcp
     {
       name: "gcp/basic",
-      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/gcp/basic.json`,
+      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/gcp/basic.jsonc`,
     },
     {
       name: "gcp/airflow-cnpg",
-      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/gcp/airflow-cnpg.json`,
+      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/gcp/airflow-cnpg.jsonc`,
     },
     {
       name: "gcp/cnpg",
-      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/gcp/cnpg.json`,
+      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/gcp/cnpg.jsonc`,
     },
     {
       name: "gcp/hop",
-      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/gcp/hop.json`,
+      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/gcp/hop.jsonc`,
     },
     // azure
     {
       name: "azure/basic",
-      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/azure/basic.json`,
+      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/azure/basic.jsonc`,
     },
     {
       name: "azure/airflow-cnpg",
-      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/azure/airflow-cnpg.json`,
+      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/azure/airflow-cnpg.jsonc`,
     },
     {
       name: "azure/cnpg",
-      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/azure/cnpg.json`,
+      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/azure/cnpg.jsonc`,
     },
     {
       name: "azure/hop",
-      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/azure/hop.json`,
+      url: `${POLYSEAM_TEMPLATE_DIRECTORY}/azure/hop.jsonc`,
     },
   ];
   return knownTemplates;
