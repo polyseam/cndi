590e050add2dac7a59eb1d2f00c360c4b2ca6b83
IamTamika <tamika.taylor@untribe.com>
2023-06-29 14:21:59 -0600

    ch path

--- src/outputs/gitignore.ts
+++ src/outputs/gitignore.ts
@@ -7,7 +7,8 @@ cndi/terraform/.terraform*
 cndi/terraform/*.tfstate*
 cndi/terraform/.terraform/
 .DS_Store
-microk8s-cloud-init-leader-hardcoded-values.yml.tftpl
+cndi/terraform/microk8s-cloud-init-leader-hardcoded-values.yml.tftpl
+cndi/terraform/leader_ip_address.txt
 `;
   return gitignoreContents.trim();
 }
