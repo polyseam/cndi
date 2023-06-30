c6899eed15379d0555db4aac4076734646286f92
IamTamika <tamika.taylor@untribe.com>
2023-06-30 15:05:23 -0600

    rm extra res that get node ip from staging

--- src/outputs/terraform/dev/stageAll.ts
+++ src/outputs/terraform/dev/stageAll.ts
@@ -7,16 +7,14 @@ import provider from "./provider.tf.json.ts";
 import terraform from "./terraform.tf.json.ts";
 import cndi_dev_locals from "./locals.tf.json.ts";
 import cndi_multipass_instance from "./cndi_multipass_instance.tf.json.ts";
-import cndi_data_local_file from "./cndi_data_local_file.tf.json.ts";
 import cndi_local_sensitive_file from "./cndi_local_sensitive_file.tf.json.ts";
 import cndi_outputs from "./cndi_outputs.tf.json.ts";
-import cndi_terraform_data from "./cndi_terraform_data.tf.json.ts";
 
 const devStageAllLabel = ccolors.faded(
   "\nsrc/outputs/terraform/dev/stageAll.ts:",
 );
 
-export default async function stageTerraformResourcesForGCP(
+export default async function stageTerraformResourcesForDev(
   config: CNDIConfig,
 ) {
   const node = config.infrastructure.cndi.nodes[0];
@@ -41,20 +39,12 @@ export default async function stageTerraformResourcesForGCP(
         cndi_multipass_instance(node),
       ),
       stageFile(
-        path.join("cndi", "terraform", "cndi_data_local_file.tf.json"),
-        cndi_data_local_file(),
-      ),
-      stageFile(
         path.join("cndi", "terraform", "cndi_local_sensitive_file.tf.json"),
         cndi_local_sensitive_file(),
       ),
       stageFile(
         path.join("cndi", "terraform", "cndi_outputs.tf.json"),
         cndi_outputs(node),
-      ),
-      stageFile(
-        path.join("cndi", "terraform", "cndi_terraform_data.tf.json"),
-        cndi_terraform_data(node),
       ),
     ]);
   } catch (e) {
