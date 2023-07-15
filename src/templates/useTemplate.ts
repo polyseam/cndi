ff7af5e10b80df5775837391285889ab674847af
harshil <harshil4076@gmail.com>
2023-07-14 22:13:42 -0400

    ran deno build

--- src/templates/useTemplate.ts
+++ src/templates/useTemplate.ts
@@ -134,9 +134,9 @@ function replaceRange(
 ) {
   //For number type add 1 to end index and reduce 1 from start index to remove "".
   //So instead of it returning "members": "3" it will return "members": 3
-  return typeof(substitute) ==="number"?
-  s.substring(0, start -1) + substitute + s.substring(end + 1):
-  s.substring(0, start) + substitute + s.substring(end)
+  return typeof (substitute) === "number"
+    ? s.substring(0, start - 1) + substitute + s.substring(end + 1)
+    : s.substring(0, start) + substitute + s.substring(end);
 }
 
 // returns a string where templated values are replaced with their literal values from prompt responses
