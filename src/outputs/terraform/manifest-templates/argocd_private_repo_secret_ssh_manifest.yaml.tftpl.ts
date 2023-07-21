d28d3ede84ef5bc1c4849ee77bea2697c9d16539
Matt Johnston <m@ttjohnston.com>
2023-07-21 15:51:24 -0300

    use yaml literal instead of json conversion for root ssh app

--- src/outputs/terraform/manifest-templates/argocd_private_repo_secret_ssh_manifest.yaml.tftpl.ts
+++ src/outputs/terraform/manifest-templates/argocd_private_repo_secret_ssh_manifest.yaml.tftpl.ts
@@ -1,21 +1,16 @@
-import { YAML } from "deps";
-
 export default function getArgoPrivateRepoSecretSSHYamlTftpl() {
-  const clusterRepoSecretSSH = {
-    apiVersion: "v1",
-    kind: "Secret",
-    metadata: {
-      name: "private-repo",
-      namespace: "argocd",
-      labels: {
-        "argocd.argoproj.io/secret-type": "repository",
-      },
-    },
-    stringData: {
-      type: "git",
-      sshPrivateKey: "\${git_ssh_private_key}",
-      url: "\${git_repo}",
-    },
-  };
-  return YAML.stringify(clusterRepoSecretSSH).trim();
+  const clusterRepoSecretSSHYAML = `
+apiVersion: v1
+kind: Secret
+metadata:
+  name: private-repo
+  namespace: argocd
+  labels:
+    argocd.argoproj.io/secret-type: repository
+stringData:
+  type: git
+  sshPrivateKey: |-
+  	"\${git_ssh_private_key}"
+  url: "\${git_repo}"`;
+  return clusterRepoSecretSSHYAML.trim();
 }
