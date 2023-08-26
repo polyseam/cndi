142311039be4271c26989c03cdc379650774c5e0
IamTamika <tamika.taylor@untribe.com>
2023-08-26 16:48:33 -0600

    fix cluster output

--- src/outputs/terraform/azure-aks/stageAll.ts
+++ src/outputs/terraform/azure-aks/stageAll.ts
@@ -1,7 +1,6 @@
 import { ccolors, path } from "deps";
-import { AzureAKSNodeItemSpec } from "src/types.ts";
+import { AzureAKSNodeItemSpec, CNDIConfig } from "src/types.ts";
 import { emitExitEvent, stageFile } from "src/utils.ts";
-
 import provider from "./provider.tf.json.ts";
 import terraform from "./terraform.tf.json.ts";
 import cndi_azurerm_resource_group from "./cndi_azurerm_resource_group.tf.json.ts";
@@ -10,13 +9,24 @@ import cndi_outputs from "./cndi_outputs.tf.json.ts";
 import cndi_aks_cluster from "./cndi_aks_cluster.tf.json.ts";
 import cndi_azurerm_public_ip_lb from "./cndi_azurerm_public_ip_lb.tf.json.ts";
 export default async function stageTerraformResourcesForAzureAKS(
-  node: AzureAKSNodeItemSpec,
+  config: CNDIConfig,
 ) {
   const azure_location = (Deno.env.get("ARM_REGION") as string) || "eastus";
 
+  const stageNodes = config.infrastructure.cndi.nodes.map((node) =>
+    stageFile(
+      path.join(
+        "cndi",
+        "terraform",
+        `cndi_aks_cluster_${node.name}.tf.json`,
+      ),
+      cndi_aks_cluster(node as AzureAKSNodeItemSpec),
+    )
+  );
   // stage all the terraform files at once
   try {
     await Promise.all([
+      ...stageNodes,
       stageFile(
         path.join("cndi", "terraform", "provider.tf.json"),
         provider(),
