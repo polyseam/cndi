b0135b862d193c01ee1df343b1ff749c2030c99d
IamTamika <tamika.taylor@untribe.com>
2023-09-15 09:06:58 -0600

    hard code storage class and  rm firewall

--- src/outputs/terraform/gcp-gke/stageAll.ts
+++ src/outputs/terraform/gcp-gke/stageAll.ts
@@ -23,7 +23,6 @@ import cndi_argocd_root_application_manifest from "./cndi_argocd_root_applicatio
 import cndi_sealed_secrets_secret_manifest from "./cndi_sealed_secrets_secret_manifest.tf.json.ts";
 import cndi_firestore_csi_storage_class_manifest from "./cndi_firestore_csi_storage_class_manifest.tf.json.ts";
 import getStorageClassManifestYamlTftpl from "src/outputs/terraform/manifest-templates/firestore_csi_storage_class_manifest.yaml.tftpl.ts";
-import cndi_google_compute_firewall_internal from "./cndi_google_compute_firewall_internal.tf.json.ts";
 import getSealedSecretsKeyYamlTftpl from "src/outputs/terraform/manifest-templates/sealed_secrets_secret_manifest.yaml.tftpl.ts";
 import getArgoAdminPasswordSecretManifestYamlTftpl from "src/outputs/terraform/manifest-templates/argocd_admin_password_secret_manifest.yaml.tftpl.ts";
 import getArgoPrivateRepoSecretHTTPSYamlTftpl from "src/outputs/terraform/manifest-templates/argocd_private_repo_secret_https_manifest.yaml.tftpl.ts";
@@ -310,14 +309,6 @@ export default async function stageTerraformResourcesForGCPGKE(
           "firestore_csi_storage_class_manifest.tf.json",
         ),
         cndi_firestore_csi_storage_class_manifest(),
-      ),
-      stageFile(
-        path.join(
-          "cndi",
-          "terraform",
-          "cndi_google_compute_firewall_internal.tf.json",
-        ),
-        cndi_google_compute_firewall_internal(),
       ),
     ]);
   } catch (e) {
--- src/outputs/terraform/manifest-templates/firestore_csi_storage_class_manifest.yaml.tftpl.ts
+++ src/outputs/terraform/manifest-templates/firestore_csi_storage_class_manifest.yaml.tftpl.ts
@@ -4,15 +4,11 @@ apiVersion: storage.k8s.io/v1
 kind: StorageClass
 metadata:
   name: nfs
-  annotations:
-    storageclass.kubernetes.io/is-default-class: 'true'
 provisioner: filestore.csi.storage.gke.io
 parameters:
   tier: standard
-  network: '\${vpc_id}'
-  connect-mode: PRIVATE_SERVICE_ACCESS
 reclaimPolicy: Delete
 allowVolumeExpansion: true
-volumeBindingMode: WaitForFirstConsumer
+volumeBindingMode: Immediate
 `.trim();
 }
