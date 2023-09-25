5172de1d3bb645f2606673a4bf70f73bd73fe353
IamTamika <tamika.taylor@untribe.com>
2023-09-25 12:16:31 -0600

    ch cycle

--- src/outputs/terraform/aws-eks/cndi_aws_eks_cluster.tf.json.ts
+++ src/outputs/terraform/aws-eks/cndi_aws_eks_cluster.tf.json.ts
@@ -13,7 +13,7 @@ export default function getAWSEKSClusterTFJSON(
   const instance_type = node?.instance_type || DEFAULT_INSTANCE_TYPES.aws;
   // const node_group_name = node.name;
   const module = getTFModule("aws_eks_cluster", {
-    cluster_name: "${local.cluster_name}",
+    cluster_name: "${local.cndi_project_name}",
     cluster_endpoint_public_access: true,
     cluster_version: "1.27",
     manage_aws_auth_configmap: false,
