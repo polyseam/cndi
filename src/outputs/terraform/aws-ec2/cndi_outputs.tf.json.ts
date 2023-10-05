e067dc354c38afa787e78e0584702cfb06e265a0
IamTamika <tamika.taylor@untribe.com>
2023-10-05 13:01:18 -0600

    test output

--- src/outputs/terraform/aws-ec2/cndi_outputs.tf.json.ts
+++ src/outputs/terraform/aws-ec2/cndi_outputs.tf.json.ts
@@ -9,6 +9,12 @@ export default function getOutputTFJSON(): string {
         value:
           "${replace(aws_lb.cndi_aws_lb.dns_name,local.aws_region,upper(local.aws_region))}",
       },
+      node: {
+        // GitHub Actions Secrets will redact the domain is it contains the secret AWS_REGION
+        // so we need to replace the secret with the same value in uppercase
+        value:
+          '${replace(aws_instance.cndi_aws_instance_x-airflow-node.private_dns,".ec2.internal", "")}',
+      },
       resource_group: {
         value:
           "https://${upper(local.aws_region)}.console.aws.amazon.com/resource-groups/group/CNDIResourceGroup_${local.cndi_project_name}?region=${upper(local.aws_region)}",
