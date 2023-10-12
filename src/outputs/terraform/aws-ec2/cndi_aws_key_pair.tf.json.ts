1d40b54994d80576005915bff9c8f01a3f250c14
IamTamika <tamika.taylor@untribe.com>
2023-10-12 14:38:14 -0600

    ch to key prefix to avoid dups

--- src/outputs/terraform/aws-ec2/cndi_aws_key_pair.tf.json.ts
+++ src/outputs/terraform/aws-ec2/cndi_aws_key_pair.tf.json.ts
@@ -2,7 +2,7 @@ import { getPrettyJSONString, getTFResource } from "src/utils.ts";
 
 export default function getAWSAwsKeyPairTFJSON(): string {
   const resource = getTFResource("aws_key_pair", {
-    key_name: "cndi_aws_key_pair",
+    key_name_prefix: "ssh_access_key",
     public_key: "${var.ssh_access_public_key}",
   });
   return getPrettyJSONString(resource);
