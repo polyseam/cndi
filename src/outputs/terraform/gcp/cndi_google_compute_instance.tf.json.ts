538f52f96e510d486f93ac9e8dba001c3dedb39c
Matt Johnston <m@ttjohnston.com>
2023-10-12 17:40:44 -0300

    attempt metadata ssh-keys for gce

--- src/outputs/terraform/gcp/cndi_google_compute_instance.tf.json.ts
+++ src/outputs/terraform/gcp/cndi_google_compute_instance.tf.json.ts
@@ -49,6 +49,7 @@ export default function getGCPComputeInstanceTFJSON(
       machine_type,
       metadata: {
         "user-data": user_data,
+        "ssh-keys": "ubuntu:${var.ssh_access_public_key}}",
       },
       name,
       network_interface,
