464ab9a3a5374de2163692dd3543fda7e3e17a76
IamTamika <tamika.taylor@untribe.com>
2023-10-20 16:55:45 -0600

    ch name convention

--- src/outputs/terraform/azure/cndi_azurerm_resource_group.tf.json.ts
+++ src/outputs/terraform/azure/cndi_azurerm_resource_group.tf.json.ts
@@ -3,7 +3,7 @@ import { getPrettyJSONString, getTFResource } from "src/utils.ts";
 export default function getAzureResourceGroupTFJSON(): string {
   const resource = getTFResource("azurerm_resource_group", {
     location: "${local.azure_location}",
-    name: "${local.cndi_project_name}",
+    name: "rg-${local.cndi_project_name}",
     tags: { CNDIProject: "${local.cndi_project_name}" },
   });
   return getPrettyJSONString(resource);
