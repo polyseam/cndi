663a1f32382a65db77afa12a2e0483d14b45025d
Matt Johnston <m@ttjohnston.com>
2023-11-15 18:27:39 -0400

    rg in microk8s stack instead of aws core

--- src/outputs/terraform/aws/AWSCoreStack.ts
+++ src/outputs/terraform/aws/AWSCoreStack.ts
@@ -32,29 +32,5 @@ export default class AWSCoreTerraformStack extends CNDITerraformStack {
         },
       ],
     });
-
-    const query = JSON.stringify({
-      ResourceTypeFilters: ["AWS::AllSupported"],
-      TagFilters: [
-        {
-          Key: "CNDIProject",
-          Values: [`${project_name}`],
-        },
-      ],
-    });
-
-    new CDKTFProviderAWS.resourcegroupsGroup.ResourcegroupsGroup(
-      this,
-      `cndi_aws_resource_group`,
-      {
-        name: `CNDIResourceGroup_${project_name}`,
-        tags: {
-          Name: "CNDIResourceGroup_${local.cndi_project_name}",
-        },
-        resourceQuery: {
-          query,
-        },
-      },
-    );
   }
 }
