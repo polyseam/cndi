c64b4a700ee653d5382e6547a89fbc16799edf2f
IamTamika <tamika.taylor@untribe.com>
2023-11-15 09:59:05 -0700

    revert to hardcoded values

--- src/outputs/terraform/azure/cndi_azurerm_subnet.tf.json.ts
+++ src/outputs/terraform/azure/cndi_azurerm_subnet.tf.json.ts
@@ -3,7 +3,7 @@ import { getPrettyJSONString, getTFResource } from "src/utils.ts";
 export default function getAzureSubnetTFJSON(): string {
   const resource = getTFResource("azurerm_subnet", {
     address_prefixes: [
-      "${cidrsubnet(azurerm_virtual_network.cndi_azurerm_virtual_network.address_space[0], 8, 0)}",
+      "10.0.0.0/24",
     ],
     name: "cndi-subnet-${local.cndi_project_name}",
     private_endpoint_network_policies_enabled: true,
--- src/outputs/terraform/azure/cndi_azurerm_virtual_network.tf.json.ts
+++ src/outputs/terraform/azure/cndi_azurerm_virtual_network.tf.json.ts
@@ -3,7 +3,7 @@ import { getPrettyJSONString, getTFResource } from "src/utils.ts";
 export default function getAzureVirtualNetworkTFJSON(): string {
   const resource = getTFResource("azurerm_virtual_network", {
     address_space: [
-      "${cidrsubnet(10.0.0.0/8, 4, random_id.cndi_random_integer_vnet_suffix.dec)}",
+      "10.0.0.0/16",
     ],
     location: "${azurerm_resource_group.cndi_azurerm_resource_group.location}",
     name: "cndi-virtual-network-${local.cndi_project_name}",
