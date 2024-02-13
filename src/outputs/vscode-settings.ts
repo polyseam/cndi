2af2453cd4a78ac9d965714c1acf49c89e6dc026
Matt Johnston <m@ttjohnston.com>
2024-02-12 21:33:18 -0400

    jsonschema updates

--- src/outputs/vscode-settings.ts
+++ src/outputs/vscode-settings.ts
@@ -2,13 +2,6 @@ const settings = {
   "files.associations": {
     "*.sh.tftpl": "shellscript",
   },
-  "json.schemas": [
-    {
-      fileMatch: ["*cndi_config.json*"],
-      url:
-        "https://raw.githubusercontent.com/polyseam/cndi/main/src/schemas/cndi_config.schema.json",
-    },
-  ],
   "yaml.schemas": {
     "https://raw.githubusercontent.com/polyseam/cndi/main/src/schemas/cndi_config.schema.json":
       ["cndi_config.yaml"],
