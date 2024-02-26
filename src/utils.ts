17ab822f7123d7dc2c4a7f315b27ec987af34184
Matt Johnston <m@ttjohnston.com>
2024-02-26 18:43:57 -0400

    ow no conf - 500

--- src/utils.ts
+++ src/utils.ts
@@ -66,7 +66,7 @@ const getPathToCndiConfig = async (providedPath?: string): Promise<string> => {
           ccolors.user_input(`"${providedPath}"`),
         ].join(" "),
         {
-          cause: 4500,
+          cause: 500,
         },
       );
     }
@@ -85,7 +85,7 @@ const getPathToCndiConfig = async (providedPath?: string): Promise<string> => {
         ccolors.error("file in your current directory"),
       ].join(" "),
       {
-        cause: 5000,
+        cause: 500,
       },
     );
   }
