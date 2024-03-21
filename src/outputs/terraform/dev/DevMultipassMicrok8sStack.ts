6855403fcdbf4109fe622cf10e2b4861871d2cad
Matt Johnston <m@ttjohnston.com>
2024-03-21 17:03:50 -0300

    ssh shares templates

--- src/outputs/terraform/dev/DevMultipassMicrok8sStack.ts
+++ src/outputs/terraform/dev/DevMultipassMicrok8sStack.ts
@@ -51,7 +51,7 @@ export class DevMultipassMicrok8sStack extends CNDITerraformStack {
 
     if (useSshRepoAuth()) {
       userData = Fn.templatefile(
-        "microk8s-cloud-init-leader-ssh.yml.tftpl",
+        "microk8s-cloud-init-leader.yml.tftpl",
         {
           bootstrap_token: this.locals.bootstrap_token.asString!,
           git_repo_encoded: Fn.base64encode(
