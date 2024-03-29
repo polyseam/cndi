082d4679f84c3bbf39666ad076f84725ce29d76b
Matt Johnston <m@ttjohnston.com>
2024-03-29 03:33:37 -0300

    specifiers

--- src/cliffy-provider-gh-releases/deps.ts
+++ src/cliffy-provider-gh-releases/deps.ts
@@ -1,25 +1,30 @@
 // cliffy
-export { Provider } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/upgrade/mod.ts";
+export { Provider } from "@cliffy/cliffy/command/upgrade/mod.ts";
+
 export type {
   GithubProviderOptions,
   GithubVersions,
   UpgradeOptions,
-} from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/upgrade/mod.ts";
-export { colors } from "https://deno.land/x/cliffy@v1.0.0-rc.3/ansi/colors.ts";
-export { homedir } from "node:os";
+} from "@cliffy/cliffy/command/upgrade/mod.ts";
+
+export { colors } from "@cliffy/cliffy/ansi/colors.ts";
+
 // std
+export { homedir } from "node:os";
 export { Spinner } from "@std/cli/spinner";
 export { type SpinnerOptions } from "@std/cli/spinner";
 import { compare } from "@std/semver/compare";
 import { tryParse } from "@std/semver";
+
 export const semver = {
   compare,
   tryParse,
 };
+
 export { ensureDirSync, walkSync } from "@std/fs";
 
 // github
-export { Octokit } from "https://esm.sh/octokit?dts";
+export { Octokit } from "npm:octokit";
 
 // homegrown
-export { inflateResponse } from "https://deno.land/x/inflate_response@v1.1.0/mod.ts";
+export { inflateResponse } from "@polyseam/inflate-response";
