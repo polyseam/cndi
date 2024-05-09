4f40c4521354bfd9f4b04c630feaf7c6af4c9ffc
Matt Johnston <m@ttjohnston.com>
2024-05-08 23:09:06 -0300

    fmt workflow files

--- src/outputs/cndi-onpull-workflow.ts
+++ src/outputs/cndi-onpull-workflow.ts
@@ -1,6 +1,58 @@
 import { YAML } from "deps";
 
-const generalSteps = [
+// these are somewhat brittle, they are pinned to how checkov outputs empty markdown results
+const NO_CHECKOV_FAILURES_EXPRESSION =
+  "${{ steps.checkov.outputs.results == '\n\n---' }}";
+const CHECKOV_FAILURES_EXPRESSION =
+  "${{ steps.checkov.outputs.results != '\n\n---' }}";
+
+const cndiCheckovSteps = [
+  [
+    {
+      name: "Test with Checkov",
+      id: "checkov",
+      uses: "bridgecrewio/checkov-action@master",
+      "continue-on-error": true,
+      with: {
+        directory: "./cndi", // run on all cndi artifacts
+        output_format: "github_failed_only", // github markdown of failed checks
+        output_file_path: "console,checkov", // Save results to ./checkov and print to console
+        skip_check: "CKV_SECRET_6", // Skip check for hardcoded secrets by entropy (we encrypt them)
+      },
+    },
+    {
+      name: "Comment Checkov Issues",
+      if: CHECKOV_FAILURES_EXPRESSION,
+      uses: "thollander/actions-comment-pull-request@v2",
+      with: {
+        mode: "recreate",
+        comment_tag: "checkov-failures-comment",
+        message: `## Checkov Failures
+          
+Checkov found issues in your pull request. Please review and fix them.
+
+\${{ steps.checkov.outputs.results }}`,
+      },
+    },
+    {
+      name: "Delete Comment Checkov",
+      if: NO_CHECKOV_FAILURES_EXPRESSION,
+      uses: "thollander/actions-comment-pull-request@v2",
+      with: {
+        mode: "delete",
+        comment_tag: "checkov-failures-comment",
+        message: "Checkov found no issues",
+      },
+    },
+    {
+      name: "Print Checkov Results",
+      if: NO_CHECKOV_FAILURES_EXPRESSION,
+      run: 'echo "Checkov found no issues"',
+    },
+  ],
+];
+
+const runCndiReleaseSteps = [
   {
     name: "welcome",
     run: 'echo "welcome to cndi!"',
@@ -47,144 +99,83 @@ const generalSteps = [
     },
     run: "cndi ow",
   },
-  {
-    name: "Test Terraform with Checkov",
-    id: "checkov-tf",
-    uses: "bridgecrewio/checkov-action@master",
-    with: {
-      directory: "cndi/terraform",
-      framework: "terraform_json",
-      output_format: "github_failed_only",
-      output_file_path: "checkov-tf.md",
-    },
-  },
-  {
-    name: "Test Kubernetes with Checkov",
-    id: "checkov-k8s",
-    uses: "bridgecrewio/checkov-action@master",
-    with: {
-      directory: "cndi/cluster_manifests",
-      framework: "kubernetes",
-      output_format: "github_failed_only",
-      output_file_path: "checkov-k8s.md",
-    },
-  },
-  {
-    name: "Comment Checkov K8s",
-    uses: "thollander/actions-comment-pull-request@v2",
-    "continue-on-error": true,
-    with: {
-      filePath: "./checkov-k8s.md",
-    },
-  },
-  {
-    name: "Comment Checkov Terraform",
-    uses: "thollander/actions-comment-pull-request@v2",
-    "continue-on-error": true,
-    with: {
-      filePath: "./checkov-tf.md",
-    },
-  },
 ];
 
-function getSteps(sourceRef?: string) {
+function getCndiExecutionSteps(sourceRef?: string) {
   if (!sourceRef) {
-    return generalSteps;
+    return runCndiReleaseSteps;
   }
 
-  return [{
-    name: "welcome",
-    run: `echo "welcome to cndi@${sourceRef}!"`,
-  }, {
-    name: "checkout cndi repo",
-    uses: "actions/checkout@v3",
-    with: {
-      repository: "polyseam/cndi",
-      "fetch-depth": 0,
-      ref: sourceRef,
+  const buildAndRunCndiSteps = [
+    {
+      name: "welcome",
+      run: `echo "welcome to cndi@${sourceRef}!"`,
     },
-  }, {
-    name: "setup deno",
-    uses: "denoland/setup-deno@v1",
-  }, {
-    name: "build cndi",
-    run: "deno task build-linux",
-  }, {
-    name: "persist cndi",
-    run: "mkdir -p $HOME/.cndi/bin && mv ./dist/linux/in/* $HOME/.cndi/bin/",
-  }, {
-    name: "checkout repo",
-    uses: "actions/checkout@v3",
-    with: {
-      "fetch-depth": 0,
+    {
+      name: "checkout cndi repo",
+      uses: "actions/checkout@v3",
+      with: {
+        repository: "polyseam/cndi",
+        "fetch-depth": 0,
+        ref: sourceRef,
+      },
     },
-  }, {
-    name: "Setup Python",
-    uses: "actions/setup-python@v4",
-    with: {
-      "python-version": "3.8",
+    {
+      name: "setup deno",
+      uses: "denoland/setup-deno@v1",
     },
-  }, {
-    name: "setup cndi",
-    uses: "polyseam/setup-cndi@v2",
-  }, {
-    name: "cndi ow",
-    env: {
-      ARM_REGION: "${{ vars.ARM_REGION }}",
-      AWS_REGION: "${{ vars.AWS_REGION }}",
-      GIT_USERNAME: "${{ secrets.GIT_USERNAME }}",
-      GIT_TOKEN: "${{ secrets.GIT_TOKEN }}",
-      GIT_SSH_PRIVATE_KEY: "${{ secrets.GIT_SSH_PRIVATE_KEY }}",
-      SSH_PUBLIC_KEY: "${{ secrets.SSH_PUBLIC_KEY }}",
-      TERRAFORM_STATE_PASSPHRASE: "${{ secrets.TERRAFORM_STATE_PASSPHRASE }}",
-      SEALED_SECRETS_PRIVATE_KEY: "${{ secrets.SEALED_SECRETS_PRIVATE_KEY }}",
-      SEALED_SECRETS_PUBLIC_KEY: "${{ secrets.SEALED_SECRETS_PUBLIC_KEY }}",
-      ARGOCD_ADMIN_PASSWORD: "${{ secrets.ARGOCD_ADMIN_PASSWORD }}",
-      AWS_ACCESS_KEY_ID: "${{ secrets.AWS_ACCESS_KEY_ID }}",
-      AWS_SECRET_ACCESS_KEY: "${{ secrets.AWS_SECRET_ACCESS_KEY }}",
-      GOOGLE_CREDENTIALS: "${{ secrets.GOOGLE_CREDENTIALS }}",
-      ARM_SUBSCRIPTION_ID: "${{ secrets.ARM_SUBSCRIPTION_ID }}",
-      ARM_TENANT_ID: "${{ secrets.ARM_TENANT_ID }}",
-      ARM_CLIENT_ID: "${{ secrets.ARM_CLIENT_ID }}",
-      ARM_CLIENT_SECRET: "${{ secrets.ARM_CLIENT_SECRET }}",
-      CNDI_TELEMETRY: "${{ secrets.CNDI_TELEMETRY }}",
+    {
+      name: "build cndi",
+      run: "deno task build-linux",
     },
-    run: "cndi ow",
-  }, {
-    name: "Test Terraform with Checkov",
-    id: "checkov-tf",
-    uses: "bridgecrewio/checkov-action@master",
-    with: {
-      directory: "cndi/terraform",
-      framework: "terraform_json",
-      output_format: "github_failed_only",
-      output_file_path: "checkov-tf.md",
+    {
+      name: "persist cndi",
+      run: "mkdir -p $HOME/.cndi/bin && mv ./dist/linux/in/* $HOME/.cndi/bin/",
     },
-  }, {
-    name: "Test Kubernetes with Checkov",
-    id: "checkov-k8s",
-    uses: "bridgecrewio/checkov-action@master",
-    with: {
-      directory: "cndi/cluster_manifests",
-      framework: "kubernetes",
-      output_format: "github_failed_only",
-      output_file_path: "checkov-k8s.md",
+    {
+      name: "checkout repo",
+      uses: "actions/checkout@v3",
+      with: {
+        "fetch-depth": 0,
+      },
     },
-  }, {
-    name: "Comment Checkov K8s",
-    uses: "thollander/actions-comment-pull-request@v2",
-    "continue-on-error": true,
-    with: {
-      filePath: "./checkov-k8s.md",
+    {
+      name: "Setup Python",
+      uses: "actions/setup-python@v4",
+      with: {
+        "python-version": "3.8",
+      },
     },
-  }, {
-    name: "Comment Checkov Terraform",
-    uses: "thollander/actions-comment-pull-request@v2",
-    "continue-on-error": true,
-    with: {
-      filePath: "./checkov-tf.md",
+    {
+      name: "setup cndi",
+      uses: "polyseam/setup-cndi@v2",
     },
-  }];
+    {
+      name: "cndi ow",
+      env: {
+        ARM_REGION: "${{ vars.ARM_REGION }}",
+        AWS_REGION: "${{ vars.AWS_REGION }}",
+        GIT_USERNAME: "${{ secrets.GIT_USERNAME }}",
+        GIT_TOKEN: "${{ secrets.GIT_TOKEN }}",
+        GIT_SSH_PRIVATE_KEY: "${{ secrets.GIT_SSH_PRIVATE_KEY }}",
+        SSH_PUBLIC_KEY: "${{ secrets.SSH_PUBLIC_KEY }}",
+        TERRAFORM_STATE_PASSPHRASE: "${{ secrets.TERRAFORM_STATE_PASSPHRASE }}",
+        SEALED_SECRETS_PRIVATE_KEY: "${{ secrets.SEALED_SECRETS_PRIVATE_KEY }}",
+        SEALED_SECRETS_PUBLIC_KEY: "${{ secrets.SEALED_SECRETS_PUBLIC_KEY }}",
+        ARGOCD_ADMIN_PASSWORD: "${{ secrets.ARGOCD_ADMIN_PASSWORD }}",
+        AWS_ACCESS_KEY_ID: "${{ secrets.AWS_ACCESS_KEY_ID }}",
+        AWS_SECRET_ACCESS_KEY: "${{ secrets.AWS_SECRET_ACCESS_KEY }}",
+        GOOGLE_CREDENTIALS: "${{ secrets.GOOGLE_CREDENTIALS }}",
+        ARM_SUBSCRIPTION_ID: "${{ secrets.ARM_SUBSCRIPTION_ID }}",
+        ARM_TENANT_ID: "${{ secrets.ARM_TENANT_ID }}",
+        ARM_CLIENT_ID: "${{ secrets.ARM_CLIENT_ID }}",
+        ARM_CLIENT_SECRET: "${{ secrets.ARM_CLIENT_SECRET }}",
+        CNDI_TELEMETRY: "${{ secrets.CNDI_TELEMETRY }}",
+      },
+      run: "cndi ow",
+    },
+  ];
+  return buildAndRunCndiSteps;
 }
 
 const getWorkflowYaml = (sourceRef?: string, disable = false) => {
@@ -206,7 +197,7 @@ const getWorkflowYaml = (sourceRef?: string, disable = false) => {
           GIT_REPO: "${{ secrets.GIT_REPO }}",
           CNDI_TELEMETRY: "${{ secrets.CNDI_TELEMETRY }}",
         },
-        steps: getSteps(sourceRef),
+        steps: [...getCndiExecutionSteps(sourceRef), ...cndiCheckovSteps],
       },
     },
   };
