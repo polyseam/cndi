2b13e4909aabbcc4f73ab487bb1b599ef9a7d3a1
Matt Johnston <m@ttjohnston.com>
2024-05-09 00:25:44 -0300

    no cndi ow on pr

--- src/outputs/cndi-onpull-workflow.ts
+++ src/outputs/cndi-onpull-workflow.ts
@@ -52,134 +52,8 @@ Checkov found issues in your pull request. Please review and fix them.
   },
 ];
 
-const runCndiReleaseSteps = [
-  {
-    name: "welcome",
-    run: 'echo "welcome to cndi!"',
-  },
-  {
-    name: "checkout repo",
-    uses: "actions/checkout@v3",
-    with: {
-      "fetch-depth": 0,
-    },
-  },
-  {
-    name: "Setup Python",
-    uses: "actions/setup-python@v4",
-    with: {
-      "python-version": "3.8",
-    },
-  },
-  {
-    name: "setup cndi",
-    uses: "polyseam/setup-cndi@v2",
-  },
-  {
-    name: "cndi ow",
-    env: {
-      ARM_REGION: "${{ vars.ARM_REGION }}",
-      AWS_REGION: "${{ vars.AWS_REGION }}",
-      GIT_USERNAME: "${{ secrets.GIT_USERNAME }}",
-      GIT_TOKEN: "${{ secrets.GIT_TOKEN }}",
-      GIT_SSH_PRIVATE_KEY: "${{ secrets.GIT_SSH_PRIVATE_KEY }}",
-      SSH_PUBLIC_KEY: "${{ secrets.SSH_PUBLIC_KEY }}",
-      TERRAFORM_STATE_PASSPHRASE: "${{ secrets.TERRAFORM_STATE_PASSPHRASE }}",
-      SEALED_SECRETS_PRIVATE_KEY: "${{ secrets.SEALED_SECRETS_PRIVATE_KEY }}",
-      SEALED_SECRETS_PUBLIC_KEY: "${{ secrets.SEALED_SECRETS_PUBLIC_KEY }}",
-      ARGOCD_ADMIN_PASSWORD: "${{ secrets.ARGOCD_ADMIN_PASSWORD }}",
-      AWS_ACCESS_KEY_ID: "${{ secrets.AWS_ACCESS_KEY_ID }}",
-      AWS_SECRET_ACCESS_KEY: "${{ secrets.AWS_SECRET_ACCESS_KEY }}",
-      GOOGLE_CREDENTIALS: "${{ secrets.GOOGLE_CREDENTIALS }}",
-      ARM_SUBSCRIPTION_ID: "${{ secrets.ARM_SUBSCRIPTION_ID }}",
-      ARM_TENANT_ID: "${{ secrets.ARM_TENANT_ID }}",
-      ARM_CLIENT_ID: "${{ secrets.ARM_CLIENT_ID }}",
-      ARM_CLIENT_SECRET: "${{ secrets.ARM_CLIENT_SECRET }}",
-      CNDI_TELEMETRY: "${{ secrets.CNDI_TELEMETRY }}",
-    },
-    run: "cndi ow",
-  },
-];
-
-function getCndiExecutionSteps(sourceRef?: string) {
-  if (!sourceRef) {
-    return runCndiReleaseSteps;
-  }
-
-  const buildAndRunCndiSteps = [
-    {
-      name: "welcome",
-      run: `echo "welcome to cndi@${sourceRef}!"`,
-    },
-    {
-      name: "checkout cndi repo",
-      uses: "actions/checkout@v3",
-      with: {
-        repository: "polyseam/cndi",
-        "fetch-depth": 0,
-        ref: sourceRef,
-      },
-    },
-    {
-      name: "setup deno",
-      uses: "denoland/setup-deno@v1",
-    },
-    {
-      name: "build cndi",
-      run: "deno task build-linux",
-    },
-    {
-      name: "persist cndi",
-      run: "mkdir -p $HOME/.cndi/bin && mv ./dist/linux/in/* $HOME/.cndi/bin/",
-    },
-    {
-      name: "checkout repo",
-      uses: "actions/checkout@v3",
-      with: {
-        "fetch-depth": 0,
-      },
-    },
-    {
-      name: "Setup Python",
-      uses: "actions/setup-python@v4",
-      with: {
-        "python-version": "3.8",
-      },
-    },
-    {
-      name: "setup cndi",
-      uses: "polyseam/setup-cndi@v2",
-    },
-    {
-      name: "cndi ow",
-      env: {
-        ARM_REGION: "${{ vars.ARM_REGION }}",
-        AWS_REGION: "${{ vars.AWS_REGION }}",
-        GIT_USERNAME: "${{ secrets.GIT_USERNAME }}",
-        GIT_TOKEN: "${{ secrets.GIT_TOKEN }}",
-        GIT_SSH_PRIVATE_KEY: "${{ secrets.GIT_SSH_PRIVATE_KEY }}",
-        SSH_PUBLIC_KEY: "${{ secrets.SSH_PUBLIC_KEY }}",
-        TERRAFORM_STATE_PASSPHRASE: "${{ secrets.TERRAFORM_STATE_PASSPHRASE }}",
-        SEALED_SECRETS_PRIVATE_KEY: "${{ secrets.SEALED_SECRETS_PRIVATE_KEY }}",
-        SEALED_SECRETS_PUBLIC_KEY: "${{ secrets.SEALED_SECRETS_PUBLIC_KEY }}",
-        ARGOCD_ADMIN_PASSWORD: "${{ secrets.ARGOCD_ADMIN_PASSWORD }}",
-        AWS_ACCESS_KEY_ID: "${{ secrets.AWS_ACCESS_KEY_ID }}",
-        AWS_SECRET_ACCESS_KEY: "${{ secrets.AWS_SECRET_ACCESS_KEY }}",
-        GOOGLE_CREDENTIALS: "${{ secrets.GOOGLE_CREDENTIALS }}",
-        ARM_SUBSCRIPTION_ID: "${{ secrets.ARM_SUBSCRIPTION_ID }}",
-        ARM_TENANT_ID: "${{ secrets.ARM_TENANT_ID }}",
-        ARM_CLIENT_ID: "${{ secrets.ARM_CLIENT_ID }}",
-        ARM_CLIENT_SECRET: "${{ secrets.ARM_CLIENT_SECRET }}",
-        CNDI_TELEMETRY: "${{ secrets.CNDI_TELEMETRY }}",
-      },
-      run: "cndi ow",
-    },
-  ];
-  return buildAndRunCndiSteps;
-}
-
-const getWorkflowYaml = (sourceRef?: string, disable = false) => {
-  const on = disable ? {} : {
+const getWorkflowYaml = () => {
+  const on = {
     pull_request: {
       types: ["opened", "synchronize", "reopened"],
     },
@@ -197,10 +71,7 @@ const getWorkflowYaml = (sourceRef?: string, disable = false) => {
           GIT_REPO: "${{ secrets.GIT_REPO }}",
           CNDI_TELEMETRY: "${{ secrets.CNDI_TELEMETRY }}",
         },
-        steps: [
-          ...getCndiExecutionSteps(sourceRef), // run `cndi ow` because it should pass
-          ...cndiCheckovSteps, // run checkov
-        ],
+        steps: cndiCheckovSteps,
       },
     },
   };
