a23a52ca76d8a4c35c30bfb1ae11fbaa1719d069
Matt Johnston <m@ttjohnston.com>
2024-05-28 15:11:28 -0300

    no required cluster .env() cliffy calls

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -32,17 +32,17 @@ const runCommand = new Command()
   .env(
     "SEALED_SECRETS_PRIVATE_KEY=<value:string>",
     "Private key used to encrypt your sealed secrets.",
-    { required: true },
+    { required: false },
   )
   .env(
     "SEALED_SECRETS_PUBLIC_KEY=<value:string>",
     "Public key used to encrypt your sealed secrets.",
-    { required: true },
+    { required: false },
   )
   .env(
     "ARGOCD_ADMIN_PASSWORD=<value:string>",
     "Password used to authenticate to the ArgoCD UI.",
-    { required: true },
+    { required: false },
   )
   .env(
     "GIT_USERNAME=<value:string>",
