e75c0cbef95b638f66e46a22d2f969f496d782d4
IamTamika <tamika.taylor@untribe.com>
2024-06-19 19:26:47 -0600

    set dependency that the eksm be created first

--- src/outputs/terraform/aws/AWSEKSStack.ts
+++ src/outputs/terraform/aws/AWSEKSStack.ts
@@ -288,6 +288,9 @@ export default class AWSEKSTerraformStack extends AWSCoreTerraformStack {
       this,
       "cndi_kubernetes_storage_class_efs",
       {
+        dependsOn: [
+          eksm,
+        ],
         metadata: {
           name: "rwm",
           annotations: {
@@ -312,6 +315,9 @@ export default class AWSEKSTerraformStack extends AWSCoreTerraformStack {
       this,
       "cndi_kubernetes_storage_class_ebs",
       {
+        dependsOn: [
+          eksm,
+        ],
         metadata: {
           name: "rwo",
           annotations: {
@@ -438,6 +444,9 @@ export default class AWSEKSTerraformStack extends AWSCoreTerraformStack {
       this,
       "cndi_kubernetes_secret_sealed_secrets_key",
       {
+        dependsOn: [
+          eksm,
+        ],
         type: "kubernetes.io/tls",
         metadata: {
           name: "sealed-secrets-key",
