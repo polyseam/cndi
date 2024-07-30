46b4e0c0ca3071a8bb099d8d790bb57878d1cf6d
Matt Johnston <m@ttjohnston.com>
2024-07-30 01:09:55 -0300

    let tmp dirs be garbage collected; add ow tests

--- src/tests/commands/ow_test.ts
+++ src/tests/commands/ow_test.ts
@@ -1,37 +1,122 @@
 import { assert } from "test-deps";
 
-import { path, YAML } from "deps";
-
 import { runCndi } from "src/tests/helpers/run-cndi.ts";
 
-import { hasSameFilesAfter } from "src/tests/helpers/util.ts";
+import {
+  listChangedFilePaths,
+  setsAreEquivalent,
+} from "src/tests/helpers/util.ts";
 
-import { copyDir } from "src/utils.ts";
+import { loadCndiConfig } from "src/utils.ts";
+import { YAML } from "src/deps.ts";
 
 Deno.env.set("CNDI_TELEMETRY", "debug");
+
+const ogDir = Deno.cwd();
+
+const cleanup = () => {
+  Deno.chdir(ogDir);
+};
 
 Deno.test(
   "'cndi ow' should do nothing if no config or inputs have changed",
   async (t) => {
+    console.log("deno working directory beginning", Deno.cwd());
     let dir = "";
     await t.step("setup", async () => {
+      console.log("initializing a cndi project");
       dir = await Deno.makeTempDir();
       Deno.chdir(dir);
-      await copyDir(
-        path.join(Deno.cwd(), "tests", "mocks", "projects", "eks-basic"),
-        dir,
-      );
+      await runCndi("init", "-t", "basic", "-l", "aws/eks");
     });
 
     await t.step("test", async () => {
-      assert(await hasSameFilesAfter(async () => {
+      const changedFilePaths = await listChangedFilePaths(async () => {
+        console.log("overwriting with the same config");
         const { status } = await runCndi("ow");
         assert(status.success);
-      }));
+      });
+      assert(changedFilePaths.length === 0);
     });
-    await t.step("cleanup", async () => {
-      Deno.chdir("..");
-      await Deno.remove(dir, { recursive: true });
+
+    await t.step("cleanup", cleanup);
+  },
+);
+
+Deno.test(
+  "'cndi ow' should create a manifest file if one has been added to config",
+  async (t) => {
+    let dir = "";
+    await t.step("setup", async () => {
+      console.log("initializing a cndi project");
+      dir = await Deno.makeTempDir();
+      Deno.chdir(dir);
+      await runCndi("init", "-t", "basic", "-l", "aws/eks");
     });
+
+    await t.step("test", async () => {
+      const changedFilePaths = await listChangedFilePaths(async () => {
+        const { config } = await loadCndiConfig(dir);
+        config.cluster_manifests["new-ns"] = {
+          kind: "Namespace",
+          metadata: {
+            name: "new-ns",
+          },
+        };
+        await Deno.writeTextFile(
+          `${dir}/cndi_config.yaml`,
+          YAML.stringify(config),
+        );
+        await runCndi("ow");
+      });
+      assert(changedFilePaths.length === 1);
+    });
+
+    await t.step("cleanup", cleanup);
+  },
+);
+
+Deno.test(
+  "'cndi ow' should bootstrap functions if source typescript is provided",
+  async (t) => {
+    const filePathsCreatedForFunctions = new Set([
+      "cndi/functions/Dockerfile",
+      "cndi/functions/src/hello/index.ts",
+      "cndi/functions/src/main/index.ts",
+      "cndi/cluster_manifests/fns-pull-secret.yaml",
+      "cndi/cluster_manifests/fns-deployment.yaml",
+      "cndi/cluster_manifests/fns-env-secret.yaml",
+      "cndi/cluster_manifests/fns-namespace.yaml",
+      "cndi/cluster_manifests/fns-service.yaml",
+      ".github/workflows/cndi-fns.yaml",
+      "functions/hello/index.ts",
+    ]);
+
+    let dir = "";
+    await t.step("setup", async () => {
+      console.log("initializing a cndi project");
+      dir = await Deno.makeTempDir();
+      Deno.chdir(dir);
+      await runCndi("init", "-t", "basic", "-l", "aws/eks");
+    });
+
+    await t.step("test", async () => {
+      const changedFilePaths = await listChangedFilePaths(async () => {
+        await Deno.mkdir(`${dir}/functions/hello`, { recursive: true });
+        await Deno.writeTextFile(
+          `${dir}/functions/hello/index.ts`,
+          `Deno.serve(() => (new Response('', { status: 200 })));`,
+          { create: true },
+        );
+        await runCndi("ow");
+      });
+      const changedFilePathsSet = new Set(changedFilePaths);
+
+      assert(
+        setsAreEquivalent(filePathsCreatedForFunctions, changedFilePathsSet),
+      );
+    });
+
+    await t.step("cleanup", cleanup);
   },
 );
--- src/tests/deps.ts
+++ src/tests/deps.ts
@@ -1,3 +1,4 @@
 export { beforeEach, describe, it } from "@std/testing/bdd";
 export { assert, assertRejects, assertThrows } from "@std/assert";
 export { parse as parseDotEnv } from "@std/dotenv";
+export { walk } from "@std/fs";
