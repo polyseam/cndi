903cefb4f04bf3cf03fe25f76aa34f81c2eaa493
Matt Johnston <m@ttjohnston.com>
2024-08-08 00:23:47 -0300

    revert to values string

--- src/outputs/application-manifest.ts
+++ src/outputs/application-manifest.ts
@@ -83,7 +83,7 @@ const getApplicationManifest = (
   releaseName: string,
   applicationSpec: CNDIApplicationSpec,
 ): [string, string] => {
-  const valuesObject = applicationSpec?.values || {};
+  const values = getYAMLString(applicationSpec?.values || {});
   const specSourcePath = applicationSpec.path;
   const specSourceChart = applicationSpec.chart;
 
@@ -133,7 +133,7 @@ const getApplicationManifest = (
       targetRevision,
       helm: {
         version: DEFAULT_HELM_VERSION,
-        valuesObject,
+        values,
       },
     },
     destination: {
