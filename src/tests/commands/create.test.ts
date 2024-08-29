dce68dc1ceb2f68162dbf6dc8f7b0e71e932427d
Matt Johnston <m@ttjohnston.com>
2024-08-29 00:24:46 -0300

    test: cndi create publishes repo successfully

--- src/tests/commands/create.test.ts
+++ src/tests/commands/create.test.ts
@@ -1,8 +1,17 @@
-import { assert } from "test-deps";
+import { assert, getProjectRoot, loadDotEnv, path } from "test-deps";
 
 import { runCndi } from "src/tests/helpers/run-cndi.ts";
 
 Deno.env.set("CNDI_TELEMETRY", "debug");
+
+const { GIT_TOKEN } = loadDotEnv({
+  export: true,
+  envPath: path.join(getProjectRoot(), ".env"),
+});
+
+Deno.env.set("GH_TOKEN", GIT_TOKEN);
+
+const decoder = new TextDecoder("utf-8");
 
 const ogDir = Deno.cwd();
 
@@ -32,6 +41,58 @@ Deno.test(
 
       assert(result.status.success === false);
       assert(result.stderrOutput.includes("slug"));
+    });
+
+    await t.step("cleanup", cleanup);
+  },
+);
+
+Deno.test(
+  "'cndi create' should create a repo if called correctly",
+  async (t) => {
+    let dir = "";
+
+    await t.step("setup", async () => {
+      dir = await Deno.makeTempDir();
+      Deno.chdir(dir);
+    });
+
+    const repoName = `test-eks-${dir.split(path.SEPARATOR).pop()}`;
+
+    await t.step("test", async () => {
+      const result = await runCndi(
+        "create",
+        `polyseam/${repoName}`,
+        "-l",
+        "aws/eks",
+        "-t",
+        "basic",
+        "--non-interactive",
+        "--loud",
+      );
+
+      assert(result.status.success);
+    });
+
+    await t.step("clean-repo", () => {
+      const command = new Deno.Command("gh", {
+        args: [
+          "repo",
+          "delete",
+          `polyseam/${repoName}`,
+          "--yes",
+        ],
+        stderr: "piped",
+        stdout: "piped",
+      });
+
+      const { code, stderr, stdout } = command.outputSync();
+
+      if (code !== 0) {
+        console.error(decoder.decode(stderr));
+      } else {
+        console.log(decoder.decode(stdout));
+      }
     });
 
     await t.step("cleanup", cleanup);
--- src/tests/deps.ts
+++ src/tests/deps.ts
@@ -2,3 +2,8 @@ export { beforeEach, describe, it } from "@std/testing/bdd";
 export { assert, assertRejects, assertThrows } from "@std/assert";
 export { parse as parseDotEnv } from "@std/dotenv";
 export { walk } from "@std/fs";
+export { loadSync as loadDotEnv } from "@std/dotenv";
+export { default as getProjectRoot } from "get-project-root";
+import { join, SEPARATOR } from "@std/path";
+const path = { join, SEPARATOR };
+export { path };
