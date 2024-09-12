03d9424ff3f47e2d8c3cfc1ac4cbae1ce775c5f3
IamTamika <tamika.taylor@untribe.com>
2024-09-11 18:32:16 -0600

    fmt

--- src/outputs/terraform/manifest-templates/rwm_storage_class.yaml.tftpl.ts
+++ src/outputs/terraform/manifest-templates/rwm_storage_class.yaml.tftpl.ts
@@ -1,19 +1,19 @@
 import { YAML } from "deps";
 
 export default function getStorageClassManifestYamlTftpl() {
-    const rwmStorageclass = {
-        apiVersion: "storage.k8s.io/v1",
-        allowVolumeExpansion: true,
-        kind: "StorageClass",
-        metadata: {
-            name: "rwm",
-            annotations: {
-                "storageclass.kubernetes.io/is-default-class": "false",
-            },
-        },
-        provisioner: "cluster.local/nfs-server-provisioner",
-        reclaimPolicy: "Delete",
-        volumeBindingMode: "Immediate",
-    };
-    return YAML.stringify(rwmStorageclass).trim();
+  const rwmStorageclass = {
+    apiVersion: "storage.k8s.io/v1",
+    allowVolumeExpansion: true,
+    kind: "StorageClass",
+    metadata: {
+      name: "rwm",
+      annotations: {
+        "storageclass.kubernetes.io/is-default-class": "false",
+      },
+    },
+    provisioner: "cluster.local/nfs-server-provisioner",
+    reclaimPolicy: "Delete",
+    volumeBindingMode: "Immediate",
+  };
+  return YAML.stringify(rwmStorageclass).trim();
 }
