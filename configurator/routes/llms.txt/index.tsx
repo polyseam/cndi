2d172601646813927c674a7579bd90695c1fb0fd
Matt Johnston <m@ttjohnston.com>
2025-02-21 01:15:00 -0400

    wip:

--- configurator/routes/llms.txt/index.tsx
+++
@@ -1,6 +0,0 @@
-import { Handlers } from "$fresh/server.ts";
-
-export const handler: Handlers = {
-  GET: (_req, _ctx) =>
-    fetch("https://raw.githubusercontent.com/polyseam/cndi/main/llms.txt"),
-};
