b4b818cbf8807dfe6a78b8dabfb9546392dd752c
Matt Johnston <m@ttjohnston.com>
2025-03-19 20:58:13 -0300

    CNDI_TELEMETRY=none during tests

--- src/tests/commands/init/env.test.ts
+++ src/tests/commands/init/env.test.ts
@@ -4,35 +4,6 @@ import { path } from "deps";
 
 import { runCndi } from "src/tests/helpers/run-cndi.ts";
 
-// describe("cndi init debug flag", () => {
-//   it("should set CNDI_TELEMETRY=debug in .env when using -d flag", async () => {
-//     const cwd = await Deno.makeTempDir();
-//
-//     // TODO: This is unvalidated, CNDI_TELEMETRY was set above
-//     // Setup
-//     Deno.env.delete("CNDI_TELEMETRY");
-//
-//     // Test
-//     /* const { status } = */ await runCndi({
-//       args: [
-//         "init",
-//         "-t",
-//         "airflow",
-//         "-d",
-//         "--set",
-//         "deployment_target_provider=aws",
-//       ],
-//       cwd,
-//     });
-//     const dotenv = await Deno.readTextFile(path.join(cwd, `.env`));
-//     assert(dotenv.indexOf(`CNDI_TELEMETRY=debug`) > -1);
-//     // assert(status.success);
-//
-//     // Cleanup
-//     // Deno.env.set("CNDI_TELEMETRY", "debug");
-//   });
-// });
-
 describe("cndi init environment variables", () => {
   it("generates a .env file with AWS credentials for airflow template", async () => {
     const cwd = await Deno.makeTempDir();
