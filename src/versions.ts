8b964ecb111c98e15239084b70ca39c3d49f75f2
Matt Johnston <m@ttjohnston.com>
2025-05-30 03:13:17 -0300

    update argocd chart; external-dns chart;

--- src/versions.ts
+++ src/versions.ts
@@ -16,7 +16,7 @@ export const EDGE_RUNTIME_IMAGE_TAG = "1.66.5";
 export const DEFAULT_K8S_VERSION = "1.31";
 
 // Root Application Chart Versions (terraform managed)
-export const ARGOCD_CHART_VERSION = "7.7.16";
+export const ARGOCD_CHART_VERSION = "8.0.12";
 export const ARGOCD_APPS_CHART_VERSION = "1.4.1";
 export const SEALED_SECRETS_CHART_VERSION = "2.17.0";
 
@@ -24,7 +24,7 @@ export const SEALED_SECRETS_CHART_VERSION = "2.17.0";
 export const CERT_MANAGER_CHART_VERSION = "1.14.3";
 export const RELOADER_CHART_VERSION = "1.0.69";
 export const NGINX_CHART_VERSION = "4.12.1";
-export const EXTERNAL_DNS_CHART_VERSION = "6.35.0";
+export const EXTERNAL_DNS_CHART_VERSION = "8.8.3";
 export const KUBE_PROMETHEUS_STACK_CHART_VERSION = "67.4.0";
 export const PROMTAIL_CHART_VERSION = "6.16.6";
 export const LOKI_CHART_VERSION = "6.24.0";
