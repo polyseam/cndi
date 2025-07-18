cec07db263d9676628b276a3136c778247d4caf6
Matt Johnston <m@ttjohnston.com>
2025-07-18 13:00:19 -0300

    deprecate microk8s

--- src/cndi_config/validate/mod.ts
+++ src/cndi_config/validate/mod.ts
@@ -129,17 +129,37 @@ function validateCNDIConfigSpecComponentMetadata(
       ccolors.warn("has the unsupported value"),
       ccolors.user_input(configSpec.cndi_version),
     );
-  } else if (!configSpec?.region && configSpec?.provider !== "dev") {
-    return new ErrOut([
-      cndiConfigFoundAtPath(pathToConfig),
-      ccolors.error("but the required"),
-      ccolors.key_name("region"),
-      ccolors.error("key is not defined"),
-    ], {
-      code: 933,
-      id: "validate/cndi_config/!region",
-      label,
-      metadata: { configSpec, pathToConfig },
-    });
+  } else {
+    if (configSpec?.provider !== "dev") {
+      if (!configSpec?.region) {
+        return new ErrOut([
+          cndiConfigFoundAtPath(pathToConfig),
+          ccolors.error("but the required"),
+          ccolors.key_name("region"),
+          ccolors.error("key is not defined"),
+        ], {
+          code: 933,
+          id: "validate/cndi_config/!region",
+          label,
+          metadata: { configSpec, pathToConfig },
+        });
+      }
+
+      if (configSpec?.distribution === "microk8s") {
+        return new ErrOut([
+          cndiConfigFoundAtPath(pathToConfig),
+          ccolors.error("but the"),
+          ccolors.key_name("distribution"),
+          ccolors.user_input("microk8s"),
+          ccolors.error("is not supported for your target provider"),
+          ccolors.user_input(configSpec?.provider),
+        ], {
+          code: 943,
+          id: "validate/cndi_config/distribution/microk8s",
+          label,
+          metadata: { provider: configSpec?.provider },
+        });
+      }
+    }
   }
 }
